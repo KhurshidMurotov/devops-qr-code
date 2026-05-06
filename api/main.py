8530d329061190635514421d61d0958e0510d416
Darius Lisbeth <xurwidbek2016@gmail.com>
2026-05-06 18:00:56 +0500

    test

--- api/main.py
+++ api/main.py
@@ -27,9 +27,10 @@ app.add_middleware(
 s3 = boto3.client(
     's3',
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"),
+    region_name=os.getenv("AWS_REGION"))
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'my-qr-generator-2026' # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
